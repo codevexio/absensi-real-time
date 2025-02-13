4a1a4038330d81baf7e1cb576f929b5804986fbe
Husainq <qalbi21si@mahasiswa.pcr.ac.id>
2025-02-14 01:26:40 +0700

    Pembaruan Database

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CutiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+Route::get('update-cuti', [CutiController::class, 'updateExpiredCuti']);
+Route::post('/cuti/update-expired', [CutiController::class, 'updateExpiredCuti']);
+Route::post('/cuti/ajukan', [CutiController::class, 'ajukanCuti']);
+Route::post('/cuti/approve/{id}', [CutiController::class, 'approveCuti']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CutiController;
 use App\Http\Controllers\Auth\AuthController;
 
 Route::view('/', 'login');
