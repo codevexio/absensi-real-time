0fabc2b8d2470020f291cbfedd2ca050475394b6
husainq <qalbi21si@mahasiswa.pcr.ac.id>
2025-05-14 13:25:45 +0700

    fix: add logging support in PengajuanCutiController

--- app/Http/Controllers/Api/PengajuanCutiController.php
+++ app/Http/Controllers/Api/PengajuanCutiController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Cuti;
 use App\Models\PengajuanCuti;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 
