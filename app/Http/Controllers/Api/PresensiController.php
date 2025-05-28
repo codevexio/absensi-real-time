9c8658e6a471e3cb4a8a4aa1ebc783a20c04193e
husainq <qalbi21si@mahasiswa.pcr.ac.id>
2025-05-28 16:11:54 +0700

    fix: update image storage method in presensiMasuk to use store

--- app/Http/Controllers/Api/PresensiController.php
+++ app/Http/Controllers/Api/PresensiController.php
@@ -176,7 +176,7 @@ class PresensiController extends Controller
             return response()->json(['message' => 'Lokasi di luar area kantor'], 400);
         }
 
-        $path = $request->file('imageMasuk')->storeAs('presensi', $filename, 'public');
+        $path = $request->file('imageMasuk')->store('presensi', 'public');
 
         // Simpan atau update presensi
         $presensi = Presensi::updateOrCreate(
