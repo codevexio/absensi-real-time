b401a0795b90453aa3583e1d4724bd6785f3d604
Husainq <qalbi21si@mahasiswa.pcr.ac.id>
2025-07-27 13:52:07 +0700

    fix: update query for PostgreSQL in listRekapPresensi method

--- app/Http/Controllers/Api/PresensiController.php
+++ app/Http/Controllers/Api/PresensiController.php
@@ -327,14 +327,11 @@ class PresensiController extends Controller
                 return response()->json(['message' => 'Karyawan belum login'], 401);
             }
 
-            // MySQL
-            // $rekap = Presensi::selectRaw("DATE_FORMAT(tanggalPresensi, '%Y-%m') as bulan")
-
-            // PostgreSQL
+            // TO_CHAR digunakan untuk PostgreSQL
             $rekap = Presensi::selectRaw("TO_CHAR(tanggalPresensi, 'YYYY-MM') as bulan")
                 ->where('karyawan_id', $user->id)
-                ->groupBy('bulan')
-                ->orderByDesc('bulan')
+                ->groupByRaw("TO_CHAR(tanggalPresensi, 'YYYY-MM')")
+                ->orderByRaw("TO_CHAR(tanggalPresensi, 'YYYY-MM') DESC")
                 ->get();
 
             return response()->json([
